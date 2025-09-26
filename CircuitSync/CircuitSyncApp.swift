fa1beb9dc8855723827eb9f009eb6a1efbe5713e
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2025-09-26 15:35:53 -0400

    Initial Commit

---
+++ CircuitSync/CircuitSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircuitSyncApp.swift
+//  CircuitSync
+//
+//  Created by Isabelle Beaudry Hajji on 2025-09-26.
+//
+
+import SwiftUI
+
+@main
+struct CircuitSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
